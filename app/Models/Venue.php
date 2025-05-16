1ee9548f934b0053f490ddda4baa2cee6fbde704
thanujsai <sboda@modeln.com>
2025-05-16 20:38:29 +0530

    blueprint stuff, creating models from draft.yaml

---
+++ app/Models/Venue.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Venue extends Model
+{
+    use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'integer',
+        ];
+    }
+
+    public function conferences(): HasMany
+    {
+        return $this->hasMany(Conference::class);
+    }
+}
